4c357cf4df010e783a7e4cfcdc36c8604aa6ea1c
Phani Kumar <phanikumar2791@gmail.com>
2025-02-14 14:24:38 +0530

    TTD_KATA: Adds calculator class

---
+++ tests/calculator.test.ts
@@ -0,0 +1,9 @@
+import { Calculator } from "../src/calculator";
+
+describe("Calculator", () => {
+  let calculator: Calculator;
+
+  beforeEach(() => {
+    calculator = new Calculator();
+  });
+});
