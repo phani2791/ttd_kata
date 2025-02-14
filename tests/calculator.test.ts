15313aea2e71f4f24ecf95a8d6cc532ca45b2865
Phani Kumar <phanikumar2791@gmail.com>
2025-02-14 16:59:31 +0530

    TTD_KATA: Adds input validations for positive numbers.

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -33,4 +33,20 @@ describe("Calculator", () => {
       calculator.add("1,,3,,4,,5,,");
     }).toThrow(INVALID_STRING_MSG);
   });
+  test("should not handle new lines between numbers", () => {
+    expect(() => {
+      calculator.add("1\n2,3");
+    }).toThrow(INVALID_STRING_MSG);
+  });
+
+  test("should not handle different delimiters", () => {
+    expect(() => {
+      calculator.add("//;\n1;2");
+    }).toThrow(INVALID_STRING_MSG);
+  });
+  test("should handle multiple delimiters", () => {
+    expect(() => {
+      calculator.add("//[*][%]\n1*2%3");
+    }).toThrow(INVALID_STRING_MSG);
+  });
 });
