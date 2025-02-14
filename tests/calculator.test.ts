5eb731a5c8e8dcc39193eaa19aafaf5c07572ffb
Phani Kumar <phanikumar2791@gmail.com>
2025-02-14 16:04:02 +0530

    TDD_KATA:Adds input validation for empty strings.

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -2,12 +2,30 @@ import { Calculator } from "../src/calculator";
 
 describe("Calculator", () => {
   let calculator: Calculator;
-
+  const INVALID_STRING_MSG =
+    "Provide valid string of numbers separated by commas";
   beforeEach(() => {
     calculator = new Calculator();
   });
 
   test("should add two numbers", () => {
     expect(calculator.add("1, 2")).toBe(3);
+  });
+  test("check for empty string", () => {
+    expect(() => {
+      calculator.add("");
+    }).toThrow(INVALID_STRING_MSG);
+  });
+
+  test("check for empty values", () => {
+    expect(() => {
+      calculator.add(",,,");
+    }).toThrow(INVALID_STRING_MSG);
+  });
+
+  test("check for min two numbers", () => {
+    expect(() => {
+      calculator.add("1,");
+    }).toThrow("Provide at least two numbers");
   });
 });
